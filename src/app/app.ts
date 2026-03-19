112a581e63351a073490f119fe7c5f3184db70f1
valisoa-rajaonarisoa <valisoarajaonarisoa27@gmail.com>
2026-03-19 09:31:35 +0300

    apprentissage des signal avec computed, effect, siganl

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 import { Employee } from './employee/employee';
 import { DatePipe } from '@angular/common';
 import { UpperCasePipe } from './upper-case-pipe';
@@ -6,13 +6,23 @@ import { UpperCasePipe } from './upper-case-pipe';
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  //ON IMPORT LE PIPE AUSS ICI LE UPPSERCASEPIPE 
-  imports: [Employee,DatePipe,UpperCasePipe],
+  //ON IMPORT LE PIPE AUSS ICI LE UPPSERCASEPIPE
+  imports: [Employee, DatePipe, UpperCasePipe],
   styleUrl: './app.css',
 })
 export class App {
-  nom = 'telephone';
-  price = 10.5;
+  //1 - D É F I N I R   U N   E T A T
+  name = signal('Valisoa');
+  age = signal(22);
 
-  now = new Date();
+  // 3 -  É C O U T E R  A U   E V E N T  ET 4 - RÉ A G I R 
+  increment() {
+    //ON PEUT UTILISER SET => POUR CHANGEEMENT ET UPDATE => ON VEUT METTRE AJOUR AVEC L'ANCIEN VALEUR
+    this.age.update((age) => age + 1);
+  }
+
+  decrement() {
+    //ON PEUT UTILISER SET => POUR CHANGEEMENT ET UPDATE => ON VEUT METTRE AJOUR AVEC L'ANCIEN VALEUR
+    this.age.update((age) => age - 1);
+  }
 }
