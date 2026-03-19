539590801f836923d1c5f5d53e6330a465950cee
valisoa-rajaonarisoa <valisoarajaonarisoa27@gmail.com>
2026-03-19 13:42:45 +0300

    apprentissage des flux et directives

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Employee } from './employee/employee';
+import { CardEmployee } from './card-employee/card-employee';
 
 export type DepartementType = 'IT' | 'Marketing' | 'HR';
 export type LevelType = 'J' | 'M' | 'S';
@@ -14,11 +15,31 @@ export interface IEmployee {
   selector: 'app-root',
   templateUrl: './app.html',
   //ON IMPORT LE PIPE AUSS ICI LE UPPSERCASEPIPE
-  imports: [Employee],
+  imports: [Employee, CardEmployee],
   styleUrl: './app.css',
 })
 export class App {
- 
+  listEmployes: IEmployee[] = [
+    {
+      id: '1245efdkhh12dddhje',
+      name: 'Jao',
+      departement: 'IT',
+      level: 'M',
+    },
+    {
+      id: '1245efzedkhh12dhje',
+      name: 'Koto',
+      departement: 'HR',
+      level: 'J',
+    },
+    {
+      id: '1245esdfafdkhh12dhje',
+      name: 'Tanjona',
+      departement: 'HR',
+      level: 'S',
+    },
+  ];
+
   employee: IEmployee = {
     id: '1245efdkhh12dhje',
     name: 'Jao',
--- src/app/employee/employee.ts
+++ src/app/employee/employee.ts
@@ -1,10 +1,11 @@
-import { Component, input, output } from '@angular/core';
+import { Component, computed, input, output } from '@angular/core';
 import { IEmployee } from '../app';
 import { LevelPipe } from '../level-pipe';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-employee',
-  imports: [LevelPipe],
+  imports: [LevelPipe, NgClass], //importer le ngClass
   templateUrl: './employee.html',
   styleUrl: './employee.css',
 })
@@ -12,6 +13,14 @@ export class Employee {
   //Récuperation  du data avec son nom nom= input(),
   //  required pour dire que c'est requis
   employee = input.required<IEmployee>();
+
+  //je cree une vraible
+  bgColor = computed(() => {
+    const level = this.employee().level;
+    if (level == 'J') return 'bg-red-200';
+    if (level == 'M') return 'bg-blue-300';
+    else return 'bg-green-300';
+  });
 
   //  O U T P U T
   //1 -  Création de l'output avec nomOutput= output<Type>() // c'est un event, le type c'est le type de retour
